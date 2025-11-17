f4b5c348e87b2ea793f6098890d7229b889eb7fe
Tibi011 <megsztil@gmail.com>
2025-11-17 14:14:44 +0100

    elso vegpont

--- backend/backend.js
+++ backend/backend.js
@@ -19,7 +19,28 @@ app.get('/', (req, res) => {
         })
 
     //Tibi backend
+    //Évek lekérdezése
+    //Feladatok lekérdezése év id alapján (POST)
     
+    app.post('/feladatKerdezId', (req, res) => {
+        const {feladat_ev} =req.body
+        const sql=`
+                    select *
+                    from feladat
+                    where feladat.feladat_ev= ?
+                    `
+        pool.query(sql,[feladat_ev], (err, result) => {
+        if (err) {
+            console.log(err)
+            return res.status(500).json({error:"Hiba"})
+        }
+        if (result.length===0){
+            return res.status(404).json({error:"Nincs adat"})
+        }
+
+        return res.status(200).json(result)
+        })
+})
 
     //Bogi backend
 
