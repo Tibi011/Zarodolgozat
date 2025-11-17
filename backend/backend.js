5ec107a6653c89aadf4f2a00146da9294fb23fbb
Tibi011 <megsztil@gmail.com>
2025-11-17 13:08:33 +0100

    backend

---
+++ backend/backend.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const mysql = require('mysql')
+const cors = require('cors')
+const app = express()
+const port = 3000
+
+app.use(cors())
+app.use(express.json())
+
+const pool = mysql.createPool({
+        host: 'localhost',
+        user: 'root',
+        password: '',
+        database: ''
+        })
+
+    //Tibi backend
+
+
+    //Bogi backend
+
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
+
+
